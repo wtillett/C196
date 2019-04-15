46f4c6e191d3a54c21bc771d1acc20d2db1e2cf8
Will Tillett <wtillett@gmail.com>
2019-04-15 15:59:17 -0700

    Adjust margins on landscape layout variants

--- app/src/main/java/com/wtillett/c196project/MentorDetailActivity.java
+++ app/src/main/java/com/wtillett/c196project/MentorDetailActivity.java
@@ -1,15 +1,14 @@
 package com.wtillett.c196project;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.wtillett.c196project.database.AppDatabase;
 import com.wtillett.c196project.database.Mentor;
