023e0c034c39c7a25d58fb7a671941f421c2b7d1
Will Tillett <wtillett@gmail.com>
2019-04-15 18:31:48 -0700

    Add assessment button to main activity programmatically.

--- app/src/main/java/com/wtillett/c196project/MainActivity.java
+++ app/src/main/java/com/wtillett/c196project/MainActivity.java
@@ -1,9 +1,12 @@
 package com.wtillett.c196project;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.wtillett.c196project.database.AppDatabase;
 import com.wtillett.c196project.database.Course;
@@ -16,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     private AppDatabase db;
 
     // TODO: Add application title and icon
-    // TODO: Programmatically add to the UI
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         db = AppDatabase.getDatabase(getApplicationContext());
+
+        // Add a button to the UI programmatically
+        addAssessmentButton();
+    }
+
+    private void addAssessmentButton() {
+        ConstraintLayout constraintLayout = findViewById(R.id.mainConstraintLayout);
+        Button courseButton = findViewById(R.id.courseButton);
+        Button button = new Button(this);
+        button.setId(View.generateViewId());
+        button.setText(R.string.assessment_button_text);
+        constraintLayout.addView(button);
+
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(constraintLayout);
+        constraintSet.connect(button.getId(), ConstraintSet.LEFT,
+                constraintLayout.getId(), ConstraintSet.LEFT, 28);
+        constraintSet.connect(button.getId(), ConstraintSet.RIGHT,
+                constraintLayout.getId(), ConstraintSet.RIGHT, 28);
+        constraintSet.connect(button.getId(), ConstraintSet.TOP,
+                courseButton.getId(), ConstraintSet.BOTTOM, 56);
+        constraintSet.constrainWidth(button.getId(),
+                ConstraintSet.MATCH_CONSTRAINT);
+        constraintSet.applyTo(constraintLayout);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchAssessmentActivity(v);
+            }
+        });
     }
 
     public void launchTermActivity(View view) {
